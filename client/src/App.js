e525bd044c6a1bcd1472e2222de4285c6462710c
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-11-30 15:43:25 -0800

    edit app,js to use Store and Provider

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,8 @@ import {
   createHttpLink,
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
-
+import { Provider } from 'react-redux';
+import { Store } from './utils/Store';
 import Home from './pages/Home';
 import Detail from './pages/Detail';
 import NoMatch from './pages/NoMatch';
@@ -42,7 +43,7 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
-          <StoreProvider>
+          <Provider store = {Store}>
             <Nav />
             <Switch>
               <Route exact path="/" component={Home} />
@@ -53,7 +54,7 @@ function App() {
               <Route exact path="/products/:id" component={Detail} />
               <Route component={NoMatch} />
             </Switch>
-          </StoreProvider>
+          </Provider>
         </div>
       </Router>
     </ApolloProvider>
